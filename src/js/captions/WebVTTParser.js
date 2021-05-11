8f4c497ea7f8203de39fadfaa9e7cf75444d5bc2
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-05-11 13:39:26 +0200

    WebVTT parser added

---
+++ src/js/captions/WebVTTParser.js
@@ -0,0 +1,66 @@
+
+
+const TIMESTAMP = "(?:\\d*:){1,2}\\d*";
+const CUE_TIMING = `(${TIMESTAMP})\\s*\\-\\->\\s*(${TIMESTAMP})`;
+
+const re = {
+    cueTiming: new RegExp(CUE_TIMING)
+};
+
+const parseCue = (line,i,lines) => {
+    const result = re.cueTiming.exec(line);
+    if (result) {
+        const label = lines[i - 1];
+        let captions = [];
+        for (let j = 1; i+j<lines.length && lines[i+j] !== ''; ++j) {
+            captions.push(lines[i+j]);
+        }
+        return {
+            label: label,
+            start: result[1],
+            end: result[2],
+            caption: captions
+        };
+    }
+    return null;
+}
+
+export function parseWebVTT(text) {
+    const result = {
+        cues: []
+    }
+    
+    if (text !== "") {
+        text = text.replace(/\r\n/gm,"\n");
+        text = text.replace(/\r/gm,"\n");
+
+        text.split(/\n/).forEach((line,i,lines) => {
+            let cue = parseCue(line,i,lines);
+            if (cue) {
+                result.cues.push(cue);
+            }
+        })
+    }
+
+    return result;
+}
+
+export default class WebVTTParser {
+    constructor(text = "") {
+        this._text = text;
+        this._data = parseWebVTT(text);
+    }
+
+    get text() {
+        return this._text;
+    }
+
+    set text(text) {
+        this._text = text;
+        this._data = parseWebVTT(text);
+    }
+
+    get cues() {
+        return this._data.cues;
+    }
+}
