56545146ee52cea840efe58fe70912e3452203a0
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-06-08 10:19:39 +0200

    Preview container implementation improvements

--- src/js/core/PreviewContainer.js
+++ src/js/core/PreviewContainer.js
@@ -1,20 +1,28 @@
 
-import { DomClass } from './dom';
+import { DomClass, createElementWithHtmlText } from './dom';
 
 const g_style = `
-    background-image: url("[BACKGROUND_IMAGE]");
+    background-color: #e4e4e4;
     width: 100%;
     height: 100%;
-    background-repeat: no-repeat;
-    background-size: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const g_imgStyle = `
+    width: 100%;
 `
 export default class PreviewContainer extends DomClass {
     constructor(player, parentElement,backgroundImage) {
         const attributes = {
             "class": "preview-container",
-            "style": g_style.replace('[BACKGROUND_IMAGE]', backgroundImage)
+            "style": g_style
         };
         super(player, {attributes, parent: parentElement});
+
+        this._img = createElementWithHtmlText(`
+            <img style="${g_imgStyle}" src="${backgroundImage}" alt=""/>
+        `, this.element);
 
         this.element.addEventListener("click", (evt) => {
             player.play();
@@ -22,6 +30,6 @@ export default class PreviewContainer extends DomClass {
     }
 
     loadBackgroundImage(src) {
-        this.setAttribute("style",g_style.replace('[BACKGROUND_IMAGE]',src));
+        this._img.setAttribute("src",src);
     }
 }
