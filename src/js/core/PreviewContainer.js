1f51b7c8de46eed48bac8ea2c34e594fe9db10ea
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-07-13 13:35:53 +0200

    Preview button implemented

--- src/js/core/PreviewContainer.js
+++ src/js/core/PreviewContainer.js
@@ -8,10 +8,34 @@ const g_style = `
     display: flex;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
 `
 const g_imgStyle = `
     width: 100%;
-`
+`;
+
+const g_iconContainerStyle = `
+    position: absolute; 
+    top: 0px; 
+    left: 0px; 
+    right: 0px; 
+    bottom: 0px; 
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+`;
+
+const g_iconStyle = `
+    pointer-events: none;
+    width: 20%;
+    max-width: 400px;
+    min-width: 100px;
+    opacity: 0.6;
+`;
+
+import PlayIcon from 'paella-core/icons/play_icon_fullscreen.svg';
+
 export default class PreviewContainer extends DomClass {
     constructor(player, parentElement,backgroundImage) {
         const attributes = {
@@ -21,7 +45,12 @@ export default class PreviewContainer extends DomClass {
         super(player, {attributes, parent: parentElement});
 
         this._img = createElementWithHtmlText(`
+        <div>
             <img style="${g_imgStyle}" src="${backgroundImage}" alt=""/>
+            <div style="${ g_iconContainerStyle }">
+                <i class="preview-play-icon" style="${ g_iconStyle }">${ PlayIcon }</i>
+            </div>
+        </div>
         `, this.element);
 
         this.element.addEventListener("click", (evt) => {
