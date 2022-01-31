54f452563ac96c66a5c26f00483c4607acbc5933
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-01-31 12:43:24 +0100

    Improvements in playback rate plugin

--- src/js/plugins/es.upv.paella.defaultShortcuts.js
+++ src/js/plugins/es.upv.paella.defaultShortcuts.js
@@ -38,62 +38,127 @@ export default class DefaultKeyShortcutsPlugin extends KeyShortcutPlugin {
         PopUp.HideAllPopUps(false);
     }
 
+    async decreaseSpeed() {
+        const current = await this.player.videoContainer.playbackRate();
+        let selected = 0;
+        this._validPlaybackRates.some(p => {
+            if (selected === 0) {
+                selected = p;
+            }
+            if (p<current) {
+                selected = p;
+            }
+            else {
+                return true;
+            }
+        });
+        await this.player.videoContainer.setPlaybackRate(selected);
+    }
+
+    async increaseSpeed() {
+        const pr = await this.player.videoContainer.playbackRate();
+        let selected = 0;
+        this._validPlaybackRates.some(p => {
+            if (p>pr) {
+                selected = p;
+                return true;
+            }
+        });
+        if (selected === 0) {
+            this._validPlaybackRates[this._validPlaybackRates.length - 1];
+        }
+        await this.player.videoContainer.setPlaybackRate(selected);
+    }
+
+    async load() {
+        this._validPlaybackRates = this.config.validPlaybackRates || [0.75, 1, 1.5, 2];
+        this._validPlaybackRates.sort((a,b) => a-b);
+    }
+
     async getKeys() {
         return [
             {
                 keyCode: KeyCodes.KeyK,
                 description: "Toggle play/pause",
-                action: async (event) => {
+                action: async () => {
                     await this.togglePlayPause();
+                }
+            },
+            {
+                keyCode: KeyCodes.KeyJ,
+                description: "Rewind 30 seconds",
+                action: async () => {
+                    await this.seek(-30);
+                }
+            },
+            {
+                keyCode: KeyCodes.KeyL,
+                description: "Rewind 30 seconds",
+                action: async () => {
+                    await this.seek(30);
                 }
             },
             {
                 keyCode: KeyCodes.Space,
                 description: "Toggle play/pause",
-                action: async (event) => {
+                action: async () => {
                     await this.togglePlayPause();
                 }
             },
             {
                 keyCode: KeyCodes.KeyF,
                 description: "Toggle fullscreen",
-                action: async (event) => {
+                action: async () => {
                     await this.toggleFullscreen();
                 }
             },
             {
                 keyCode: KeyCodes.ArrowLeft,
                 description: "Rewind 30 seconds",
-                action: async (event) => {
+                action: async () => {
                     await this.seek(-30);
                 }
             },
             {
                 keyCode: KeyCodes.ArrowRight,
                 description: "Forward 30 seconds",
-                action: async (event) => {
+                action: async () => {
                     await this.seek(30);
                 }
             },
             {
                 keyCode: KeyCodes.ArrowUp,
                 description: "Volume up 10%",
-                action: async (event) => {
+                action: async () => {
                     this.incrementVolume(10);
                 }
             },
             {
                 keyCode: KeyCodes.ArrowDown,
                 description: "Volume down 10%",
-                action: async (event) => {
+                action: async () => {
                     this.incrementVolume(-10);
                 }
             },
             {
                 keyCode: KeyCodes.Escape,
                 description: "Close Pop Up",
-                action: async (event) => {
+                action: async () => {
                     this.closePopUp();
+                }
+            },
+            {
+                keyCode: KeyCodes.KeyU,
+                description: "Decrease playback speed",
+                action: async () => {
+                    await this.decreaseSpeed();
+                }
+            },
+            {
+                keyCode: KeyCodes.KeyO,
+                description: "Increase playback speed",
+                action: async () => {
+                    this.increaseSpeed();
                 }
             }
         ]
