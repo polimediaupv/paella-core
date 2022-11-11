e266026eef56997215bd0ac57c2bcfb1276f49ff
Fernando Serrano Carpena <ferserc1@gmail.com>
2022-11-11 08:23:46 +0100

    Dynamic dual video layout implemented

--- src/js/layouts/es.upv.paella.dualVideoDynamic.js
+++ src/js/layouts/es.upv.paella.dualVideoDynamic.js
@@ -1,8 +1,15 @@
 
 import VideoLayout from 'paella-core/js/core/VideoLayout';
 
-import defaultIconMaximize from "../../icons/maximize.svg";
 import { CanvasButtonPosition } from '../core/CanvasPlugin';
+
+import defaultIconRotate from 'paella-core/icons/icon_rotate.svg';
+import defaultIconMinimize from 'paella-core/icons/minimize-3.svg';
+import defaultIconSwitchSide from 'paella-core/icons/icon_switch_side.svg';
+import defaultIconMaximize from 'paella-core/icons/maximize.svg';
+import defaultIconClose from 'paella-core/icons/close.svg';
+import defaultIconSideBySide from 'paella-core/icons/icon_side_by_side.svg';
+
 export default class DualVideoDynamicLayout extends VideoLayout {
     get layoutType() {
         return "dynamic";
@@ -13,15 +20,69 @@ export default class DualVideoDynamicLayout extends VideoLayout {
     }
 
     getVideoCanvasButtons(layoutStructure, content, video, videoCanvas) {
-        return [{
-            icon: defaultIconMaximize,
+        const iconMinimize = this.player.getCustomPluginIcon(this.name,"iconMinimize") || defaultIconMinimize;
+        const iconMaximize = this.player.getCustomPluginIcon(this.name,"iconMaximize") || defaultIconMaximize;
+        const iconSwitchSide = this.player.getCustomPluginIcon(this.name,"iconSwitchSide") || defaultIconRotate;
+        const iconClose = this.player.getCustomPluginIcon(this.name,"iconClose") || defaultIconClose;
+        const layoutData = () => this._currentContent.find(lo => lo.id === content);
+        const isMinimized = () => layoutData().size === 25;
+        const isMaximized = () => layoutData().size > 25;
+        const result = [];
+
+        if (isMinimized()) {
+            result.push({
+                icon: iconMaximize,
+                position: CanvasButtonPosition.LEFT,
+                title: this.player.translate('Maximize video'),
+                ariaLabel: this.player.translate('Maximize video'),
+                click: async () => {
+                    this._currentContent.forEach(lo => {
+                        lo.size = 50;
+                    });
+                    await this.player.videoContainer.updateLayout();
+                }
+            });
+        }
+        else if (isMaximized()) {
+            result.push({
+                icon: iconMinimize,
+                position: CanvasButtonPosition.LEFT,
+                title: this.player.translate('Minimize video'),
+                ariaLabel: this.player.translate('Minimize video'),
+                click: async () => {
+                    this._currentContent.forEach(lo => {
+                        lo.size = lo.id === content ? 25 : 75;
+                    });
+                    await this.player.videoContainer.updateLayout();
+                }
+            });
+        }
+        result.push({
+            icon: iconSwitchSide,
             position: CanvasButtonPosition.LEFT,
-            title: "Test button",
-            ariaLabel: "Test button",
+            title: this.player.translate('Switch side'),
+            ariaLabel: this.player.translate('Switch side'),
             click: async () => {
-                alert("Test button");
+                const ct1 = this._currentContent[0].id;
+                const ct2 = this._currentContent[1].id;
+                this._currentContent[0].id = ct2;
+                this._currentContent[1].id = ct1;
+                await this.player.videoContainer.updateLayout();
             }
-        }];
+        });
+        result.push({
+            icon: iconClose,
+            position: CanvasButtonPosition.RIGHT,
+            title: this.player.translate("Close video"),
+            ariaLabel: this.player.translate("Close video"),
+            click: async () => {
+                const singleStreamContentIds = this.player.videoContainer.validContentIds.filter(cid => cid.indexOf("-") === -1);
+                const contentId = singleStreamContentIds.find(cid => cid != content);
+                await this.player.videoContainer.setLayout(contentId);
+            }
+        });
+
+        return result;
     }
 
     getLayoutStructure(streamData, contentId) {
