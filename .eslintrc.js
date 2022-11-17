81a08680aadeea558b0a9b6c7b5e83d15292b007
Miguel Escriva <miesgre@gmail.com>
2022-11-17 19:47:07 +0100

    basic eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    ignorePatterns: ['config', 'dist', 'doc', 'node_repository'],
+    env: {
+        browser: true,
+        es2021: true,
+        es6: true,
+        node: true,
+        jest: true
+    },
+    extends: [
+        'eslint:recommended'
+        //'standard'
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: [
+    ],
+    settings: {
+        jsdoc: {
+            mode: 'typescript',
+            tagNamePreference: {
+                return: 'returns',
+                augments: 'extends'
+            }
+        }
+    },
+    rules: {
+        // enable additional rules
+        // semi: ['error', 'always'],
+        // Indent with 4 spaces
+        // indent: ['error', 4, { MemberExpression: 0 }],
+        // eqeqeq: 'off',
+        // 'space-before-function-paren': ['error', {
+        //     anonymous: 'never',
+        //     named: 'never',
+        //     asyncArrow: 'always'
+        // }],
+        // 'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+        'no-unused-vars': 'off'
+    }
+};
