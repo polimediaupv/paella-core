ec068fb80e4978a381405626b15e5a1c6a9b5551
Miguel Escriva <miesgre@gmail.com>
2023-03-15 08:11:00 +0100

    Extract language dictionaries to json files

--- src/js/plugins/es.upv.paella.defaultShortcuts.js
+++ src/js/plugins/es.upv.paella.defaultShortcuts.js
@@ -215,22 +215,4 @@ export default class DefaultKeyShortcutsPlugin extends KeyShortcutPlugin {
             }
         ]
     }
-
-    async getDictionaries() {
-        return {
-            "es": {
-                "Select the active audio track": "Seleccione la pista de audio activa",
-                "Toggle audio mute": "Conmutar el silencio de audio",
-                "Toggle play/pause": "Activar la reproducción/pausa",
-                "Toggle Captions": "Alternar subtítulos",
-                "Rewind 30 seconds": "Rebobinar 30 segundos",
-                "Forward 30 seconds": "Avanzar 30 segundos",
-                "Volume up 10%": "Aumenta el volumen un 10%.",
-                "Volume down 10%": "Reduce el volumen un 10%.",
-                "Close Pop Up": "Cerrar Pop Up",
-                "Decrease playback speed": "Reducir la velocidad de reproducción",
-                "Increase playback speed": "Aumentar la velocidad de reproducción"
-            }
-        }
-    }
 }
