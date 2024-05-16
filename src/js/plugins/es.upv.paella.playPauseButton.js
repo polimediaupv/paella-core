87555abb495ca83d58d5379bf32df4bb2581470d
Fernando Serrano Carpena <ferserc1@gmail.com>
2024-05-16 13:17:53 +0200

    Update play/pause button accessibility properties

--- src/js/plugins/es.upv.paella.playPauseButton.js
+++ src/js/plugins/es.upv.paella.playPauseButton.js
@@ -21,17 +21,32 @@ export default class PlayButtonPlugin extends ButtonPlugin {
 		const pauseIcon = this.player.getCustomPluginIcon(this.name,"pause") || defaultPauseIcon;
 		const replayIcon = this.player.getCustomPluginIcon(this.name,"replay") || defaultReplayIcon;
 		this.icon = playIcon;
+		const titlePause = this.player.translate(this.config.ariaLabelPause || "pause");
+		const titlePlay = this.player.translate(this.config.ariaLabelPlay || "play");
+		const shortcutKey = this.config.ariaKeyshortcuts || "k";
 		bindEvent(this.player, Events.PLAY, () => {
 			this.icon = pauseIcon;
+			this.button.ariaKeyshortcuts = shortcutKey;
+			this.button.ariaLabel = titlePlay;
+			this.button.title = this.config.ariaLabelPause || titlePlay;
 		});
 		bindEvent(this.player, Events.PAUSE, () => {
 			this.icon = playIcon;
+			this.button.ariaKeyshortcuts = shortcutKey;
+			this.button.ariaLabel = titlePlay;
+			this.button.title = this.config.ariaLabelPause || titlePlay;
 		});
 		bindEvent(this.player, Events.ENDED, () => {
 			this.icon = replayIcon;
+			this.button.ariaKeyshortcuts = shortcutKey;
+			this.button.ariaLabel = titlePlay;
+			this.button.title = this.config.ariaLabelPause || titlePlay;
 		});
 		bindEvent(this.player, Events.STOP, () => {
 			this.icon = playIcon;
+			this.button.ariaKeyshortcuts = shortcutKey;
+			this.button.ariaLabel = titlePlay;
+			this.button.title = this.config.ariaLabelPause || titlePlay;
 		});
 	}
 	
